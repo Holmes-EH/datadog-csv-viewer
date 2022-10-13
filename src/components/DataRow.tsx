ed79dcdbb4ce706f25257130fb006ceeb06b182a
Samuel Holmes <samuel.holmes@origins-digital.com>
2022-10-13 15:25:41 +0200

    delegate data render to rowComponent

---
+++ src/components/DataRow.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { RefactoredRowType } from "../types";
+import Message from "./Message";
+
+interface IProps {
+  filteredData: RefactoredRowType[];
+}
+
+const DataRow: FC<IProps> = ({ filteredData }): JSX.Element => {
+  const formatDate = (date: Date) => {
+    return new Intl.DateTimeFormat("fr-FR").format(date);
+  };
+  const formatTime = (date: Date) => {
+    return `${date.getHours()} h ${date.getMinutes()} s ${date.getMilliseconds()} ms`;
+  };
+  return (
+    <tbody>
+      {filteredData.map((row, index) => {
+        return (
+          <tr key={`row-${index}`}>
+            <td className="text-center">
+              {formatDate(row.date)}
+              <br />
+              {formatTime(row.date)}
+            </td>
+            <Message message={row.message} />
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
+
+export default DataRow;
