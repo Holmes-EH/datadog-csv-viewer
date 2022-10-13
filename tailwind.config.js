522c7f08b03d439bbf70a5f77a31ebc436ffd2f4
Samuel Holmes <samuel.holmes@origins-digital.com>
2022-10-13 16:06:10 +0200

    add loader and custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "origins-black": "rgb(1, 12, 24);",
+        "origins-blue": "rgb(0, 109, 206);",
+        "origins-light-orange": "rgb(255, 136, 0);",
+        "origins-med-orange": "rgb(255, 66, 17);",
+        "origins-dark-orange": "rgb(247, 59, 26);",
+        "origins-darker-orange": "rgb(232, 14, 40);",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin")],
 };
